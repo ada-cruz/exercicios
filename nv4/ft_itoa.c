eb83a33cd8c99490d315e503a2885dbd65c8d457
Aurora Da Cruz Cardoso <ada-cruz@c3r3p2.42sp.org.br>
2023-04-04 15:15:36 -0300

    itoa esta feita

---
+++ nv4/ft_itoa.c
@@ -0,0 +1,55 @@
+/*Assignment name  : ft_itoa
+Expected files   : ft_itoa.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+
+Write a function that takes an int and converts it to a null-terminated string.
+The function returns the result in a char array that you must allocate.
+
+Your function must be declared as follows:
+
+char	*ft_itoa(int nbr);*/
+
+#include <stdlib.h>
+
+char    *ft_itoa(int nbr)
+{
+    int len;
+    int n;
+    char *result;
+    // fazer etapas para pegar o tamanho do numero dado;
+    n = nbr;
+    len = 0;
+    if (nbr <= 0)
+        len++;
+    while (n)
+    {
+        n = n / 10;
+        len++;
+    }
+    // mallocar um espaço para a futura string.
+    result = malloc(sizeof (char) * (len + 1));
+    if (!result)
+        return NULL;
+    // botar o '\0' no fim do result.
+    result[len] = '\0';
+    //checar se o numero é 0.
+    if (nbr == 0)
+    {
+        result[0] = '0';
+        return (result);
+    }
+    if (nbr < 0)
+    {
+        result[0] = '-';
+        nbr = -nbr;
+    }
+    while (nbr)
+    {
+        result[--len] = nbr % 10 + '0';
+        nbr /= 10;
+    }
+    return (result);
+
+}
+
